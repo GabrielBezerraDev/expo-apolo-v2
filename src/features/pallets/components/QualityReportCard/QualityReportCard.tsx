ce2ccb4b6cc434b399bac4fccded09a332787997
GabrielBezerra <bezerrag363@gmail.com>
2026-06-26 10:19:46 -0400

    feat: enhance date formatting to include hour, minute, and second

--- src/features/pallets/components/QualityReportCard/QualityReportCard.tsx
+++ src/features/pallets/components/QualityReportCard/QualityReportCard.tsx
@@ -166,6 +166,9 @@ function formatDate(value?: string) {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
+    hour:"2-digit",
+    minute:"2-digit",
+    second:"2-digit"
   });
 }
 
